40eed588e712e5920dec194ce24856c104f442d9
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2024-03-22 22:04:51 +0100

    start create app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:szpiegula/start_screen.dart';
+
+
+void main() => runApp(SpyApp());
+
+class SpyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Colors.transparent,
+      ),
+      home: StartScreen(),
+    );
+  }
+}
+
